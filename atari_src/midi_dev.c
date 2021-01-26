1d7b1f696152f98cab6dc2e3da8a042c988fdcfd
Andreas Zdziarstek <andreas.zdziarstek@gmail.com>
2021-01-26 17:36:31 +0100

    New Atari ST port

---
+++ atari_src/midi_dev.c
@@ -0,0 +1,43 @@
+/*
+ * mt32-pi-control
+ *
+ * An mt32-pi control program for DOS PCs, Atari ST,
+ * and Amiga computers
+ *
+ * Copyright (C) 2021 Andreas Zdziarstek 
+ *
+ */
+
+#include <stdio.h>
+#include "mint/osbind.h"
+
+int mididev_init(void) {
+	return 0;
+}
+
+int mididev_deinit(void) {
+	return 0;
+}
+
+
+int mididev_send_bytes(const unsigned char *buf, int len) {
+	if(len==0) return 0;	
+	
+	if((buf[0] == 0xF0 && buf[len-1] == 0xF7) || len==3) {
+		Midiws(len-1, buf);
+	} else {
+		fprintf(stderr, "Error: Non-SysEx message with more than 3 bytes.\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+void mididev_print_usage(void) {}
+
+void mididev_add_optstr(char *optstr) {}
+
+int mididev_parse_arg(int c, const char *optarg) {
+	return 0;
+}
+
